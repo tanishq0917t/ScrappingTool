415816d610274bb0d8744f6e4422a2c856a27ab9
tanishq0917t <tanishqrawat8@gmail.com>
2025-01-20 13:53:24 +0530

    Completed

--- app/database.py
+++ app/database.py
@@ -1,14 +1,19 @@
 import json
 from .MongoConnection import MongoCollection
+from .caching import Cache
 
 class LocalStorage:
     def __init__(self, file_path: str = "products.json"):
-        self.collection=MongoCollection().getCollection()
+        self.collection = MongoCollection().getCollection()
+        self.cache = Cache()
 
     def load_data(self):
         print("Loading Data.......................")
         documents = self.collection.find({}, {"_id": 0})
-        return {doc["product_title"]: doc for doc in documents}
+        data = {doc["product_title"]: doc for doc in documents}
+        for product_title, doc in data.items():
+            self.cache.set(product_title, json.dumps(doc))
+        print(f"Loaded {len(data)} products")
 
     def save_data(self, data):
         for item in data:
@@ -16,4 +21,12 @@ class LocalStorage:
                 {"product_title": item["product_title"]},
                 {"$set": item},
                 upsert=True
+            )
+
+    def update_data(self, products):
+        for product in products:
+            product_title = product["product_title"]
+            self.collection.update_one(
+                {"product_title": product_title},
+                {"$set": product}
             )
--- app/routes.py
+++ app/routes.py
@@ -3,42 +3,44 @@ from app.auth import authenticate
 from app.scrapper import Scraper
 from app.database import LocalStorage
 from app.caching import Cache
+import json
 
 router = APIRouter()
 storage = LocalStorage()
-existing_data = storage.load_data()
+storage.load_data()
 
 @router.post("/scrape")
 def scrape(
     base_url: str = Query(..., description="Base URL to scrape from"),
-    pages: int = Query(5, description="Number of pages to scrape"),
+    pages: int = Query(..., description="Number of pages to scrape"),
     proxy: str = Query(None, description="Proxy string for scraping"),
 ):
     print(f"Base URL: {base_url}")
+    if base_url == None or pages == None or pages == 0:
+        return {"Usage":'curl -X POST "http://127.0.0.1:8000/scrape?base_url={target_url}&pages={number_of_pages}" -H "token: {static_token}"'}
+
     if base_url[-1]=='/':
         base_url=base_url[0:-1]
     scraper = Scraper(base_url=base_url, proxy=proxy)
     cache = Cache()
 
     scraped_products = []
+    update_required=[]
     
-    print(len(existing_data))
 
     for page in range(1, pages + 1):
         for product in scraper.scrape_page(page):
-            cached_price = cache.get(product["product_title"])
-            if cached_price and cached_price == product["product_price"]:
-                print("Continuing")
-                continue
-
-            cache.set(product["product_title"], product["product_price"])
-            if product["product_title"] in existing_data:
-                del existing_data[product["product_title"]]
-            scraped_products.append(product)
-            print("Adding")
-            print(f"Cached: {cached_price}   |   Price: {product['product_price']}")
-            existing_data[product["product_title"]]=product
+            cached_data = cache.get(product["product_title"])
+            if cached_data==None: #adding new product
+                cache.set(product["product_title"], json.dumps(product))
+                scraped_products.append(product)
+            else:
+                cached_data=json.loads(cached_data)
+                if cached_data['product_price'] != product["product_price"]: #updating product
+                    cache.set(product["product_title"], json.dumps(product))
+                    update_required.append(product)
 
     storage.save_data(scraped_products)
+    storage.update_data(update_required)
     print(f"Scraped {len(scraped_products)} new products.")
-    return {"scraped_products": len(scraped_products)}
+    return {"scraped_products": len(scraped_products),"updated_products":len(update_required)}
